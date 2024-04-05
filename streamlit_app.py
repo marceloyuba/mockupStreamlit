fc34779bf4cdcaff38090b6bac273d18a28ee36a
micjb <micjb.94@gmail.com>
2024-04-05 01:56:41 -0300

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,32 +12,19 @@ local_css("style/style.css")
 # ---- HEADER SECTION ----
 with st.container():    
     st.markdown('<style>h3 {color: white;}, font=</style>', unsafe_allow_html=True)
-    st.subheader("Hola, soy Marcelo Yuba :wave:")
+    st.header("Muestra parcial del Deploy de Strategic Data Transform")
     st.markdown('<style>h1 {color: white;}</style>', unsafe_allow_html=True)
-    st.title("Un Data Analyst  y Data Scientist de Buenos Aires, Argentina")
-    st.write(
-        "Soy un apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para tu negocio."
-    )
-    st.write("[Mi Github >](https://github.com/marceloyuba)")
-    st.write("[Mi LinkedIn >](https://www.linkedin.com/in/marcelo-yuba-b9a39827b/)")
+    st.title("Consultora de informacion de Latino America")
+    st.markdown('<style>h4 {color: white;}, font=</style>', unsafe_allow_html=True)
+    st.header("A continuacion mostramos el Deploy de descripcio de el proyecto, endpoints y Dashboard")
+    st.write("[Nuestro Github >](https://github.com/marceloyuba)")
+    st.write("[Nuestro LinkedIn >](https://www.linkedin.com/in/marcelo-yuba-b9a39827b/)")
 
 def Developer(desarrollador):
     resultadodeveloper = Functions.Developer(desarrollador)
     return resultadodeveloper
 
-st.markdown(""" 
-    <html>
-        <body>
-            <h2>INSTRUCCIONES</h2>
-            <p>            
-                Ingrese el destino a donde quiere ir
-                 
-            </p>      
-        </body>
-    </html>
-    """, unsafe_allow_html=True)
-
-desarrollador = st.text_input("Ingrese el destino a donde quiere ir", "Brooklyn")
+desarrollador = st.text_input("<h3>Ingrese el destino a donde quiere ir</h3>", "Brooklyn")
 if st.button("Consultar"):
     developer_result = Developer(desarrollador)
     st.write(developer_result)
