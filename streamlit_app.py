450b0359614b96f94376d7f1beca730aa1a79080
micjb <micjb.94@gmail.com>
2024-04-05 02:18:15 -0300

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,19 +35,6 @@ desarrollador = st.text_input("", "Indique el codigo de la region")
 if st.button("Consultar"):
     developer_result = Developer(desarrollador)
     st.write(developer_result)
-    
-def recomendacion_usuario(Id_Usuario):
-    resultadotop_game = Functions.recomendacion_usuario(Id_Usuario)
-    return resultadotop_game
-st.markdown('<style>h3 {color: white;}, font=</style>', unsafe_allow_html=True)
-st.markdown('<style>h2 {color: white;}, font=</style>', unsafe_allow_html=True)
-st.markdown('<style>h1 {color: white;}, font=</style>', unsafe_allow_html=True)
-st.subheader("Consulte el nivel de Co2 en su zona")
-Id_Usuario = st.text_input("", "Indique el codigo de la region")
-if st.button("Consultar_Modelo"):
-    recomendacion_result = recomendacion_usuario(Id_Usuario)
-    st.write(recomendacion_result)
-
 
 def UserForGenre(Genero_Especificado):
     resultadoReview = Functions.UserForGenre(Genero_Especificado)
