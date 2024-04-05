78db6c5f9f4bb60b6cd393169e8e66641bdfaf05
micjb <micjb.94@gmail.com>
2024-04-05 02:35:29 -0300

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ page_bg_img = f"""
 <style>
 
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://github.com/marceloyuba/PorfolioIngles/blob/main/scr/fondoTaxi.png?raw=true");
+background-image: url("https://github.com/marceloyuba/PorfolioIngles/blob/main/scr/fondoTaxi-2.png?raw=true");
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: fixed;
