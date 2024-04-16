bd6e7e4d73ebd8e2a4bbf6b0e3791363e7ab50e7
micjb <micjb.94@gmail.com>
2024-04-15 21:40:50 -0300

    Streamlitlogos

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,9 @@ st.title("")
 with st.container():
     col1, col2 = st.columns(column_widths)
 
-    st.header("Nuestra mision")
-    with col1:        
+    
+    with col1:
+        st.header("Nuestra mision")        
         st.markdown("""
                 #### Desde la expansión en 2021, Nuestro cliente analiza nuevos mercados fuera del transporte de buses, por eso nos encomendó analizar la inserción al negocio de los viajes en automóviles, analizando a su competidores directos (Taxis, Uber, Lyft) y comenzando por la ciudad de Nueva York, ya que la misma tiene una de las redes mas complejas de transporte en todo el pais, nuestro trabajo es analizar si es viable el ingreso al sistema cumpliendo con las regulaciones impuestas por el gobierno respecto a tener una ciudad libre de emisiones contaminantes""") 
     
