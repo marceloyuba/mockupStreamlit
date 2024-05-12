5cc71fe33c7c28a1ebbde3086be13833c152d865
micjb <micjb.94@gmail.com>
2024-05-12 13:48:26 -0300

    Streamlitgreyhound

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ with st.container():
     with col2:
         imagen = "scr/cliente.png"  
         st.image(imagen, width=500, use_column_width=True, output_format='auto')        
-
+st.write("<hr style='border-top: 1px solid grey;'>", unsafe_allow_html=True) 
 st.title("")
 st.write("<hr style='border-top: 1px solid grey;'>", unsafe_allow_html=True)    
 with st.container():
